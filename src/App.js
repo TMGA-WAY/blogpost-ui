fcff3da6d23116066e44806107fb4cbcf7a1376d
Mitra-babu <dipankar9612@gmail.com>
2023-10-25 00:19:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+const BASE_URL = "http://localhost:8080/";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(BASE_URL + "post/all")
+      .then((response) => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }
+      })
+      .then((data) => {
+        return data.reverse();
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
